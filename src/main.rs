d73a1cf9f5288debac125802270447be44dcb2db
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-10-16 00:59:55 +0530

    Add a check to see if the `filename` actually exists

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use clap::Parser;
 #[derive(Parser)]
 pub struct Args {
     /// The file to view
+    #[clap(default_value = "-")]
     filename: std::path::PathBuf,
 }
 
@@ -24,6 +25,16 @@ fn main() {
 
 /// The main logic of the application
 fn run(args: Args) -> Result<(), Box<dyn std::error::Error>> {
+    // Check if the file exists...
+    if !args.filename.exists() {
+        // And return early with an error if it doesn't
+        return Err(format!(
+            "The provided file does not exist: {}",
+            args.filename.to_string_lossy()
+        )
+        .into());
+    }
+
     let mut file = std::fs::File::open(&args.filename).expect("Failed to open the file");
     let mut contents: String = String::new();
     let res = file.read_to_string(&mut contents)?;
