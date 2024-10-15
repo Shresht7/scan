a736e8f0d468411d59bcbbcc4a5f4c11beb021b8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-10-16 00:39:06 +0530

    Setup `clap` for command-line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+// Traits
+use clap::Parser;
+
+/// The command-line arguments
+#[derive(Parser)]
+pub struct Args {
+    /// The file to view
+    filename: String,
+}
+
+/// The entry-point of the application
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match run(args) {
+        Ok(_) => std::process::exit(0),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    }
+}
+
+/// The main logic of the application
+fn run(args: Args) -> Result<(), Box<dyn std::error::Error>> {
+    println!("{}", args.filename);
+    Ok(())
 }
