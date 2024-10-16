13cd1da92acbf0e7cf4821e193d1200a1bff5062
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-10-16 13:57:58 +0530

    Start dividing logic of the `run` function

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod pager;
 /// The entry-point of the application
 fn main() {
     let args = cli::Args::parse();
-    match pager::Pager::run(args) {
+    match pager::Pager::init().run(args) {
         Ok(_) => std::process::exit(0),
         Err(e) => {
             eprintln!("Error: {}", e);
--- src/pager.rs
+++ src/pager.rs
@@ -4,19 +4,27 @@ use crossterm::{cursor, terminal, ExecutableCommand};
 
 use crate::cli;
 
-pub struct Pager {}
+pub struct Pager {
+    /// The index of the first-line to display
+    scroll: u32,
+    /// The max height of the page in the terminal
+    page_height: usize,
+}
 
 impl Pager {
+    /// Instantiate the Pager application
+    pub fn init() -> Pager {
+        Self {
+            scroll: 0,
+            page_height: terminal::size().unwrap_or((120, 40)).1 as usize - 1,
+        }
+    }
+
     /// The main application logic of the pager
-    pub fn run(args: cli::Args) -> Result<(), Box<dyn std::error::Error>> {
-        // Check if the file exists...
-        if !args.filename.exists() {
-            // And return early with an error if it doesn't
-            return Err(format!(
-                "The provided file does not exist: {}",
-                args.filename.to_string_lossy()
-            )
-            .into());
+    pub fn run(&mut self, args: cli::Args) -> Result<(), Box<dyn std::error::Error>> {
+        // Check if there are arguments are all valid
+        if let Err(e) = self.validate_args(&args) {
+            return Err(e); // If not, return early with the error
         }
 
         // Open the file and instantiate a BufReader
@@ -30,11 +38,6 @@ impl Pager {
         stdout.execute(terminal::Clear(terminal::ClearType::All))?;
         stdout.execute(cursor::MoveTo(0, 0))?;
 
-        // Track the position of the current line
-        let mut current_line = 0;
-        // The max height of the page in the terminal
-        let page_height = terminal::size()?.1 as usize - 1;
-
         loop {
             // Clear the screen and move the cursor to the top
             stdout.execute(terminal::Clear(terminal::ClearType::All))?;
@@ -44,13 +47,13 @@ impl Pager {
             file.seek(std::io::SeekFrom::Start(0))?; // Go back to the start of the file
             reader = std::io::BufReader::new(&file); // Reinitialize the BufReader
 
-            // Skip all lines till the start of the tracked current_line
-            for _ in 0..current_line {
+            // Skip all lines till the start of the tracked scroll position
+            for _ in 0..self.scroll {
                 let mut dummy = String::new();
                 reader.read_line(&mut dummy)?;
             }
             // Read a page's worth of lines and print them
-            for _ in 0..page_height {
+            for _ in 0..self.page_height {
                 let mut line = String::new();
                 if reader.read_line(&mut line)? == 0 {
                     break;
@@ -64,6 +67,20 @@ impl Pager {
         // Restore the terminal by exiting the Alternate Screen Buffer when we're done
         stdout.execute(terminal::LeaveAlternateScreen)?;
 
+        Ok(())
+    }
+
+    /// Validate that all arguments are as they should be
+    fn validate_args(&self, args: &cli::Args) -> Result<(), Box<dyn std::error::Error>> {
+        // Check if the file exists...
+        if !args.filename.exists() {
+            // And return early with an error if it doesn't
+            return Err(format!(
+                "The provided file does not exist: {}",
+                args.filename.to_string_lossy()
+            )
+            .into());
+        }
         Ok(())
     }
 }
