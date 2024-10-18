de5e5cb58ef4a3eae530e768c85ddee59850c722
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-10-18 13:57:27 +0530

    Create a helper function to get the visible width of an ansi string

--- src/helpers.rs
+++ src/helpers.rs
@@ -36,3 +36,51 @@ pub fn get_reader(
     };
     Ok(reader)
 }
+
+/// Character that denotes the starts of escape codes
+const ESC: char = '\x1b';
+
+pub trait ANSIString {
+    fn visible_width(&self) -> usize;
+}
+
+impl ANSIString for &str {
+    fn visible_width(&self) -> usize {
+        let mut width: usize = 0;
+        let mut chars = self.chars();
+
+        while let Some(c) = chars.next() {
+            // If we have not encountered a ESC yet ...
+            if c != ESC {
+                width += 1
+            } else {
+                // .. otherwise, we hit the start of an ESC sequence
+                while let Some(c) = chars.next() {
+                    if c == 'm' {
+                        break; // Break as soon as we encounter the end of an ansi-code
+                    }
+                }
+            }
+        }
+
+        return width;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_return_normal_width_for_regular_strings() {
+        let str = "Hello World!";
+        assert_eq!(str.len(), str.visible_width())
+    }
+
+    #[test]
+    fn should_correctly_account_for_ansi_codes() {
+        let str = "Hello World!";
+        let ansi_str = "\x1b[31mHello World!\x1b[0m";
+        assert_eq!(str.len(), ansi_str.visible_width())
+    }
+}
