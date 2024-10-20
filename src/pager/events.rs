113ab665d17b63d6bedf6b45e6c4774ed617a3f5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-10-20 22:20:28 +0530

    Move view event handlers to the view component

--- src/pager/events.rs
+++ src/pager/events.rs
@@ -1,4 +1,4 @@
-use crossterm::event::{Event, KeyCode, KeyEventKind, MouseEventKind};
+use crossterm::event::{Event, KeyCode, KeyEventKind};
 
 use super::{ui::Mode, Pager};
 use crate::helpers;
@@ -21,19 +21,15 @@ impl Pager {
         if self.command_line.handle_events(&event)? {
             return Ok(());
         }
+        if self.view.handle_events(&event, &self.lines)? {
+            return Ok(());
+        }
 
         match event {
             // It's important to check that the event is a key-press event as
             // crossterm also emits key-release and repeat events on Windows.
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 match key_event.code {
-                    KeyCode::Up | KeyCode::Char('k') => self.scroll_up(1),
-                    KeyCode::Down | KeyCode::Char('j') => self.scroll_down(1),
-                    KeyCode::Left | KeyCode::Char('h') => self.scroll_left(1),
-                    KeyCode::Right | KeyCode::Char('l') => self.scroll_right(1),
-                    KeyCode::PageUp => self.page_up(),
-                    KeyCode::PageDown => self.page_down(),
-                    KeyCode::Home => self.home(),
                     KeyCode::End => self.end(reader)?,
                     KeyCode::Esc | KeyCode::Char('q') => self.exit(),
                     KeyCode::Enter => match self.command_line.mode {
@@ -50,70 +46,11 @@ impl Pager {
                     _ => {}
                 }
             }
-            Event::Mouse(mouse_event) => match mouse_event.kind {
-                MouseEventKind::ScrollUp => self.scroll_up(1),
-                MouseEventKind::ScrollDown => self.scroll_down(1),
-                _ => {}
-            },
             Event::Resize(w, h) => self.resize(w, h, stdout)?,
             _ => {}
         }
 
         Ok(())
-    }
-
-    /// Scroll up by the given number of lines
-    fn scroll_up(&mut self, n: usize) {
-        if self.view.start() > 0 {
-            self.view.scroll_row = self.view.scroll_row.saturating_sub(n);
-        }
-    }
-
-    /// Scroll down by the given number of lines
-    fn scroll_down(&mut self, n: usize) {
-        if self.view.end() < self.lines.len() {
-            self.view.scroll_row = self.view.scroll_row.saturating_add(n);
-        }
-    }
-
-    /// Scroll left horizontally by the given number of columns
-    fn scroll_left(&mut self, n: usize) {
-        self.view.scroll_col = self.view.scroll_col.saturating_sub(n);
-    }
-
-    /// Scroll right horizontally by the given number of columns
-    fn scroll_right(&mut self, n: usize) {
-        self.view.scroll_col = self.view.scroll_col.saturating_add(n);
-    }
-
-    /// Scroll up by one page
-    fn page_up(&mut self) {
-        if self.view.start() > self.view.height {
-            self.view.scroll_row = self.view.scroll_row.saturating_sub(self.view.height - 1)
-        } else {
-            self.view.scroll_row = 0;
-        }
-    }
-
-    // Scroll down by one page
-    fn page_down(&mut self) {
-        if self.view.end() + self.view.height < self.lines.len() {
-            self.view.scroll_row = self.view.scroll_row.saturating_add(self.view.height - 1)
-        } else if self.view.end() < self.lines.len() {
-            self.view.scroll_row = self.lines.len() - self.view.height + 1;
-        }
-    }
-
-    /// Scroll to the home position.
-    /// If there is no horizontal scroll, scrolls directly to the top of the file.
-    /// Otherwise, scroll back to the start of the line first.
-    /// Next invocation will bring it back to the top if there was no horizontal scroll.
-    fn home(&mut self) {
-        if self.view.scroll_col > 0 {
-            self.view.scroll_col = 0
-        } else {
-            self.view.scroll_row = 0;
-        }
     }
 
     /// Scroll to the end position.
