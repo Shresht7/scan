0b6578b25f70a9e0adb134ba846f23e5d2fb3f61
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-10-20 11:22:08 +0530

    Refactor `command-line` and add comments

--- src/pager/ui/command_line.rs
+++ src/pager/ui/command_line.rs
@@ -2,31 +2,33 @@ use std::io::Write;
 
 use crossterm::{
     cursor,
-    event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    event::{Event, KeyCode, KeyEventKind, KeyModifiers},
     style::{style, Print, Stylize},
     terminal::{Clear, ClearType},
     QueueableCommand,
 };
 
+/// Represents the Command Line component of the Pager application.
+/// This is where the user can input their search queries and goto commands.
 #[derive(Clone, Default, PartialEq, Eq)]
 pub struct CommandLine {
     /// Stores the user input
     pub input: String,
 
-    /// The current mode of the command-line
+    /// The current [mode][Mode] of the command-line
     pub mode: Mode,
 
-    /// The x-position of the element
+    /// The x-position (column number)
     pub x: u16,
-    /// The y-position of the element
+    /// The y-position (row number)
     pub y: u16,
-
-    /// The height of the element
+    /// The height in number of rows
     pub height: usize,
-    /// The width of the element
+    /// The width in number of columns
     pub width: usize,
 }
 
+/// Describes the states the command-line can be in
 #[derive(Default, Clone, PartialEq, Eq)]
 pub enum Mode {
     #[default]
@@ -36,6 +38,10 @@ pub enum Mode {
 }
 
 impl CommandLine {
+    // SETUP
+    // -----
+
+    /// Performs the setup to initialize or re-initialize the component
     pub fn setup(&mut self, pos: (u16, u16), size: (usize, usize)) -> std::io::Result<()> {
         self.x = pos.0;
         self.y = pos.1;
@@ -44,70 +50,101 @@ impl CommandLine {
         Ok(())
     }
 
+    // RENDER
+    // ------
+
+    /// The render function is responsible for rendering the component out to the screen
     pub fn render(&self, stdout: &mut std::io::Stdout) -> std::io::Result<Self> {
+        self.render_mode(stdout)?;
+        self.render_input(stdout)?;
+        stdout.flush()?;
+        Ok(self.clone()) // Return a clone of this frame so that we can cache it and determine if we need to re-render
+    }
+
+    /// Shows the current mode of the operations of the command-line
+    fn render_mode(&self, stdout: &mut std::io::Stdout) -> std::io::Result<()> {
         let mode = match self.mode {
             Mode::Base => style(""),
             Mode::Goto => style(" GOTO ").black().on_cyan(),
             Mode::Search => style(" SEARCH ").black().on_dark_yellow(),
         };
-
         stdout
             .queue(cursor::MoveTo(self.x, self.y))?
             .queue(Clear(ClearType::CurrentLine))?
             .queue(Print(mode))?;
+        Ok(())
+    }
 
+    /// Renders the user-input on the command-line for visual feedback
+    fn render_input(&self, stdout: &mut std::io::Stdout) -> std::io::Result<()> {
         if self.input.len() > 0 {
             stdout.queue(Print(" "))?.queue(Print(&self.input))?;
         }
-
-        stdout.flush()?;
-
-        Ok(self.clone())
+        Ok(())
     }
 
+    // EVENT HANDLERS
+    // --------------
+
+    /// Event handler for the command-line component. Returns true to stop to prevent event propagation
     pub fn handle_events(&mut self, event: &Event) -> Result<bool, Box<dyn std::error::Error>> {
-        let mut stop_event_propagation = false;
-        match self.mode {
-            Mode::Search | Mode::Goto => match event {
-                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                    match key_event.code {
-                        KeyCode::Esc => {
-                            self.mode = Mode::Base;
-                            self.input.clear();
-                            stop_event_propagation = true;
-                        }
-                        KeyCode::Char(c) => {
-                            if self.mode == Mode::Goto && !c.is_numeric() {
-                                return Ok(true);
-                            }
-                            self.input.push(c)
-                        }
-                        KeyCode::Backspace => {
-                            if key_event.modifiers == KeyModifiers::CONTROL {
-                                let mut words: Vec<&str> = self.input.split(" ").collect();
-                                words.pop();
-                                self.input = words.join(" ");
-                            } else {
-                                self.input.pop();
-                            }
-                        }
-                        _ => {}
+        Ok(match self.mode {
+            Mode::Search | Mode::Goto => self.handle_search_and_goto_mode_events(event)?,
+            Mode::Base => self.handle_base_mode_events(event)?,
+        })
+    }
+
+    /// Handles events when in [Search][Mode::Search] or [Goto][Mode::Goto] mode
+    fn handle_search_and_goto_mode_events(
+        &mut self,
+        event: &Event,
+    ) -> Result<bool, Box<dyn std::error::Error>> {
+        match event {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    KeyCode::Esc => {
+                        self.mode = Mode::Base;
+                        self.input.clear();
+                        return Ok(true);
                     }
-                }
-                _ => {}
-            },
-            Mode::Base => match event {
-                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                    match key_event.code {
-                        KeyCode::Char('/') => self.mode = Mode::Search,
-                        KeyCode::Char(':') | KeyCode::Char(';') => self.mode = Mode::Goto,
-                        KeyCode::Esc => self.mode = Mode::Base,
-                        _ => {}
+                    KeyCode::Char(c) => {
+                        if self.mode == Mode::Goto && !c.is_numeric() {
+                            return Ok(true);
+                        }
+                        self.input.push(c)
                     }
+                    KeyCode::Backspace => {
+                        if key_event.modifiers == KeyModifiers::CONTROL {
+                            let mut words: Vec<&str> = self.input.split(" ").collect();
+                            words.pop();
+                            self.input = words.join(" ");
+                        } else {
+                            self.input.pop();
+                        }
+                    }
+                    _ => {}
                 }
-                _ => {}
-            },
+            }
+            _ => {}
         }
-        Ok(stop_event_propagation)
+        Ok(false)
+    }
+
+    /// Handles events when in [Base mode][Mode::Base]
+    fn handle_base_mode_events(
+        &mut self,
+        event: &Event,
+    ) -> Result<bool, Box<dyn std::error::Error>> {
+        match event {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    KeyCode::Char('/') => self.mode = Mode::Search,
+                    KeyCode::Char(':') | KeyCode::Char(';') => self.mode = Mode::Goto,
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+        Ok(false)
     }
 }
